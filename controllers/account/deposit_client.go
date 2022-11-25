6e7a6ad9bc8919300c9b12b1e40188358a3dc3ad
Muhammad Saleh Solahudin <saleh.solahudin@koinworks.com>
2022-11-26 01:31:30 +0700

    update client

--- controllers/account/deposit_client.go
+++ controllers/account/deposit_client.go
@@ -25,12 +25,31 @@ func NewDepositClient(conn *grpc.ClientConn, timeout time.Duration) DepositClien
 
 func (d DepositClient) Deposit(ctx context.Context, amount float32) (bool, error) {
 	client := pb.NewDepositServiceClient(d.conn)
-	request := &pb.DepositRequest{Amount: amount}
+	request := &pb.Req{Amount: amount}
 	ctx, cancel := context.WithDeadline(ctx, time.Now().Add(d.timeout))
 
 	defer cancel()
 
 	response, err := client.Deposit(ctx, request)
+
+	if err != nil {
+		if er, ok := status.FromError(err); ok {
+			return false, fmt.Errorf("grpc: %s, %s", er.Code(), er.Message())
+		}
+		return false, fmt.Errorf("server: %s", err.Error())
+	}
+
+	return response.GetOk(), nil
+}
+
+func (d DepositClient) Withdraw(ctx context.Context, amount float32) (bool, error) {
+	client := pb.NewDepositServiceClient(d.conn)
+	request := &pb.Req{Amount: amount}
+	ctx, cancel := context.WithDeadline(ctx, time.Now().Add(d.timeout))
+
+	defer cancel()
+
+	response, err := client.Withdraw(ctx, request)
 
 	if err != nil {
 		if er, ok := status.FromError(err); ok {
